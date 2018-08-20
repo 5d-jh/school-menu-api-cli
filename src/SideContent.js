89b4dd857b9044b863c82563d7b7f1490ab0743c
5d-jh <jeonghyen0126@gmail.com>
2018-08-20 20:04:44 +0900

    GitHub 아이콘 위치 이동

--- src/SideContent.js
+++ src/SideContent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'material-icons/iconfont/material-icons.css';
 import './SideContent.css';
+import GitHubImage from './GitHub-Mark-32px.png';
 
 let week = ['일', '월', '화', '수', '목', '금', '토'];
 
@@ -18,7 +19,7 @@ class Calendar extends Component {
         <div className="_center">
           <h1>오늘의 급식</h1>
           <a href="https://github.com/5d-jh/school-menu-api-demo-app">
-            <img src="/GitHub-Mark-32px.png" alt="repo" /> GitHub Repo
+            <img src={GitHubImage} alt="repo" /> GitHub Repo
           </a>
         </div>
       )
